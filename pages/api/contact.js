8c5900857fca8671a5ad13a2545fe3b20db98c01
Cris Fandino <sicrnujnuj@gmail.com>
2021-01-25 21:46:14 +0800

    post request for sending email

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,7 +1,26 @@
+import nodemailer from 'nodemailer'
+const transporter = nodemailer.createTransport({
+  service: 'hotmail',
+  auth: {
+    user: 'sircnujnuj@hotmail.com',
+    pass: process.env.password,
+  },
+})
+
 export default function handler(req, res) {
+  const { name, message, email } = req.body
   if (req.method === 'POST') {
-    res.redirect('/')
-  } else {
-    res.end('Hello world')
+    const mailOptions = {
+      from: 'sircnujnuj@hotmail.com',
+      to: 'sircnujnuj@gmail.com',
+      subject: `Work offer by ${name}`,
+      html: `<h2> From: ${email}</h1>
+      <p>${message}</p>
+      `,
+    }
+    transporter.sendMail(mailOptions, (err) => {
+      if (err) return res.status(400).json({ message: 'Server Error' })
+      return res.send('Email sent')
+    })
   }
 }
