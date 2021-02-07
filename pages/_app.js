4c32e116d9777b71e9be04a295546fa3986dd694
Cris Fandino <sicrnujnuj@gmail.com>
2021-02-07 20:43:45 +0800

    add a loading progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '../styles/global.scss'
 import Layout from '../components/_layout.jsx'
+import { Router } from 'next/router'
+import nProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+nProgress.configure({ showSpinner: false })
+Router.events.on('routeChangeStart', nProgress.start)
+Router.events.on('routeChangeComplete', nProgress.done)
+Router.events.on('routeChangeError', nProgress.done)
 
 function MyApp({ Component, pageProps }) {
   return (
