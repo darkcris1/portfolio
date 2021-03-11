29e50c1dcddadadb13de3b71cea9be6701bf3f58
Cris Fandino <sicrnujnuj@gmail.com>
2021-03-11 23:35:16 +0800

    add pwa assets

--- pages/_error.js
+++ pages/_error.js
@@ -1,13 +1,26 @@
 import { useRouter } from 'next/router'
 import Header from '../components/Header'
 import SEO from '../components/common/SEO'
+import { useState, useEffect } from 'react'
 
 function Error({ message = 'Page Not Found' }) {
   const { replace } = useRouter()
+  const [msg, setMsg] = useState(message)
+  const [subText, setSubtext] = useState('')
+
+  useEffect(() => {
+    const isOnline = window.navigator.onLine
+    setMsg(() => {
+      return isOnline ? msg : 'Yikes! You are offline'
+    })
+    setSubtext(() => {
+      return isOnline ? '' : 'Please refresh the page if you are connected'
+    })
+  }, [])
   return (
     <>
-      <SEO title={message} description={message} />
-      <Header title={message} subText="">
+      <SEO title={msg} description={msg} />
+      <Header title={msg} subText={subText}>
         <button className="btn primary" onClick={() => replace('/')}>
           Back to /
         </button>
